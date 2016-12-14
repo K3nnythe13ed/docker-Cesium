216a96539add9f9223e9da672b0d6f44f7b2e12a
K3nnythe13d <thegodlikez@web.de>
2016-12-14 11:33:19 +0100

    show value of locations

--- CesiumMap/script/ClickHandler.js
+++ CesiumMap/script/ClickHandler.js
@@ -75,7 +75,7 @@ function viewerEventListener() {
         var latitudeString = Cesium.Math.toDegrees(rectangleSelector.south).toFixed(2);
 
         coords.push([parseFloat(longitudeString), parseFloat(latitudeString)], [parseFloat(longitudeString2), parseFloat(latitudeString2)])
-        console.log(coords)
+        deleteDashboardWarehouseChild()
         SelectAreaLocation(coords, addToList)
         collapse()
 
@@ -128,92 +128,3 @@ function viewerEventRemoveListener() {
     screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE, Cesium.KeyboardEventModifier.ALT)
 
 }
-/* var handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
-var coordinates;
-function viewerEventListener() {
-    coordinates = [];
-    cartographic = [];
-    var scene = viewer.scene;
-    var ellipsoid = scene.globe.ellipsoid;
-    handler.setInputAction(function (movement) {
-        var mousePosition = new Cesium.Cartesian2(movement.position.x, movement.position.y);
-
-        var cartesian = viewer.camera.pickEllipsoid(mousePosition, ellipsoid);
-
-        var cartographic = ellipsoid.cartesianToCartographic(cartesian);
-        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2);
-        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2);
-             coordinates.push([parseFloat(longitudeString), parseFloat(latitudeString)]);
-    }, Cesium.ScreenSpaceEventType.LEFT_DOWN, Cesium.KeyboardEventModifier.ALT);
-
- handler.setInputAction(function (movement) {
-      var mousePosition = new Cesium.Cartesian2(movement.position.x, movement.position.y);
-
-        var cartesian = viewer.camera.pickEllipsoid(mousePosition, ellipsoid);
-
-        var cartographic = ellipsoid.cartesianToCartographic(cartesian);
-        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2);
-        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2);
-        
-            coordinates.push([parseFloat(longitudeString), parseFloat(latitudeString)]);
-
-
-            var coords = []
-            if (coordinates[0][0] < coordinates[1][0]) {
-                westLon = coordinates[0][0]
-                eastLon = coordinates[1][0]
-            }
-            else {
-                westLon = coordinates[1][0]
-                eastLon = coordinates[0][0]
-            }
-            if (coordinates[0][1] < coordinates[1][1]) {
-                southLat = coordinates[0][1]
-                northLat = coordinates[1][1]
-            }
-            else {
-                southLat = coordinates[1][1]
-                northLat = coordinates[0][1]
-            }
-            coords.push([westLon, northLat])
-            coords.push([eastLon, southLat])
-            
-        
-
-
-    }, Cesium.ScreenSpaceEventType.LEFT_UP, Cesium.KeyboardEventModifier.ALT);
-}
-
-function drawRectangle(c) {
-
-    viewer.entities.removeById('rectangleAreaSelect')
-        var selectedArea = viewer.entities.add({
-        id: 'rectangleAreaSelect',
-        name: 'Selected Area',
-        rectangle: {
-            coordinates: Cesium.Rectangle.fromDegrees(c[0][0], c[1][1], c[1][0], c[0][1]),
-            material: Cesium.Color.PURPLE.withAlpha(0.5),
-            outline: true,
-            height: 500,
-            outlineColor: Cesium.Color.GREEN
-        },
-        description: '\
-                        <p>\
-                        Selected Area:\
-                        </p>\
-                        <p>\
-                        Westernmost longitude '+ c[0][0]+' \
-                        </p>\
-                        <p>\
-                        Southernmost latitude '+c[1][1]+'\
-                        </p>\
-                        <p>\
-                        Easternmost longitude '+c[1][0]+'\
-                        </p>\
-                        <p>\
-                        Northernmost latitude '+c[0][1]+'\
-                        </p>'
-    });
-    coordinates = [];
-}
-*/
--- CesiumMap/script/SelectLocation.js
+++ CesiumMap/script/SelectLocation.js
@@ -1,7 +1,7 @@
 function SelectAreaLocation(c, addToList) {
-    deleteDashboardWarehouseChild()
-    
-    
+
+
+
     client.search({
         index: 'logstash-constant',
         type: 'warehouse',
@@ -40,13 +40,52 @@ function SelectAreaLocation(c, addToList) {
         response.hits.hits.forEach(function (hit) {
             addToList(hit, 'warehouse', false)
         })
+        InsertWarehouseValue(response)
     })
 
 }
+function InsertWarehouseValue(result) {
+    var node = document.createTextNode('Location Exp_TIV: ' + formatThousand(result.aggregations[1].value));
+    
 
+
+    var li = document.createElement("LI");
+    var att = document.createAttribute("id");
+    att.value = "WarehouseValue"
+
+    li.setAttributeNode(att);
+    li.appendChild(node);
+    var root = document.getElementById('dashboard');
+
+    root.insertBefore(li, root.childNodes[0])
+
+}
 function deleteDashboardWarehouseChild() {
     var root = document.getElementById('warehouse');
     while (root.firstChild) {
         root.removeChild(root.firstChild);
+    }
+    var ro = document.getElementById('dashboard');
+
+    if (document.getElementById('WarehouseValue')) { root.removeChild(document.getElementById('WarehouseValue')); }
+}
+
+function formatThousand(nStr) {
+    if (nStr != "undefined") {
+
+        var sep = '.';
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + sep + '$2');
+        }
+
+        return x1 + x2 + " €";
+    }
+    else {
+        return "empty";
     }
 }
